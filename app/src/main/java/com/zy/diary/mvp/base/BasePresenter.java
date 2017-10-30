f384f00fd099e3ea1d00d623e9740de7f9bb7c64
YangZhangcn <YangZhangcn>
2017-10-30 17:03:10 +0800

    init

---
+++ app/src/main/java/com/zy/diary/mvp/base/BasePresenter.java
@@ -0,0 +1,7 @@
+package com.zy.diary.mvp.base;
+
+/**
+ * Created by zhangyang on 2017/10/30.
+ */
+public interface BasePresenter {
+}
---
+++ app/src/main/java/com/zy/diary/mvp/base/BaseView.java
@@ -0,0 +1,8 @@
+package com.zy.diary.mvp.base;
+
+/**
+ * Created by zhangyang on 2017/10/30.
+ * View接口
+ */
+public interface BaseView {
+}
